148426ec43423dbe961b7955618dfd4c338fac5c
Oscar <oscarjuliandiazlopez@gmail.com>
2021-11-16 18:55:58 -0500

    Creacion RecetaActivity

---
+++ app/src/main/java/com/example/cookingassistant/ui/receta/RecetaActivity.java
@@ -0,0 +1,16 @@
+package com.example.cookingassistant.ui.receta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cookingassistant.R;
+
+public class RecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receta);
+    }
+}
